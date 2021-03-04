d012091bdaa108d32ce316b58150163e8e8fedbf
Gabriel Sória <gabrielss.rock@me.com>
2021-03-03 21:09:24 -0300

    Initial commit

---
+++ iOS/Modules/Sources/App/MainView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+public struct MainView: View {
+    
+    public init() {}
+    
+    public var body: some View {
+        Text("Hello, World").padding()
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
